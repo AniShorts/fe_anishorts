055590e5396b351b65eb85e2e15d2c25900c3bee
윤선용 <sunyong9059@naver.com>
2023-05-17 22:16:01 +0900

    header 컴포넌트 생성

---
+++ src/component/Login/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import className from "classnames/bind";
+import styles from "../../pages/Login/Introduce.module.scss"
+import { MdArrowBackIosNew } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+
+const Header = (props : {title : string}) => {
+  const navigate = useNavigate();
+  const cx = className.bind(styles);
+
+  return (
+    <div className={cx('header')}>
+        <p onClick={()=> {navigate(-1)}}><MdArrowBackIosNew /></p>
+        <p>{props.title}</p>
+    </div>
+  )
+}
+
+export default Header
