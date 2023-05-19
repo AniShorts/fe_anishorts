aed13890193da59c3741e1581c94ac34f5806cbb
윤선용 <sunyong9059@naver.com>
2023-05-20 00:04:57 +0900

    recoil 설정

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <RecoilRoot>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </RecoilRoot>
   </BrowserRouter>
 );
 
