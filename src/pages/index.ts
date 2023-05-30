69b9f23202a0548f52a8e57532ef1e4fe3c58a04
윤선용 <sunyong9059@naver.com>
2023-05-30 23:32:08 +0900

    Update index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,7 @@
 import Main from "./Main/Main";
 import Intro from "./Login/Intro";
+import SignIn from "./Login/SignIn";
+import Signup from "./Login/Signup";
+import Category from "./Login/Category";
 
-export { Main, Intro};
+export { Main, Intro, SignIn, Signup, Category };
