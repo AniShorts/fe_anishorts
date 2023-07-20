6abc332222639070fc4dafebed98516f15895534
윤선용 <sunyong9059@naver.com>
2023-07-20 21:38:22 +0900

    axios 인스턴스 생성

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,3 +1,35 @@
-import * as loginApi from "./loginApi"
+import axios from "axios";
+import { isExpired } from "react-jwt";
 
-export { loginApi }
+const BASE = process.env.REACT_APP_SERVER;
+
+const instance = axios.create({
+  baseURL: BASE,
+  headers: {
+    contentType: "application/json",
+  },
+});
+
+instance.interceptors.request.use(async (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) return config;
+    
+    if (isExpired(accessToken)) {
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const respense = await axios.post(process.env.REACT_APP_SERVER + `/auth/token`, { refreshToken });
+        localStorage.setItem("accessToken", respense.data.accessToken);
+  
+        config.headers.Authorization = `Bearer ${respense.data.accessToken}`;
+        return config;
+      } catch (error) {
+        localStorage.clear();
+        window.location.reload();
+        return config;
+      }
+    }
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  });
+
+export default instance;
