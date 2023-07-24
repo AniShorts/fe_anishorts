d4a6c7d9dfe906f331dc43a1f6afb335f04b0fed
윤선용 <sunyong9059@naver.com>
2023-07-24 22:54:47 +0900

    이름 변경으로 인한 수정

--- src/pages/Login/Category.tsx
+++ src/pages/Login/Category.tsx
@@ -4,67 +4,63 @@ import className from "classnames/bind";
 import styles from "./Category.module.scss";
 import { useRecoilValue, useRecoilState } from "recoil";
 import { userData } from "recoil/atom";
-import { loginApi } from 'apis'
+import { getCategoryApi, signupApi } from "apis/loginApi";
 
 const Category: React.FC = () => {
-const cx = className.bind(styles);
-const navigate = useNavigate();
+    const cx = className.bind(styles);
+    const navigate = useNavigate();
 
-const [animal, setAnimal] = useState([
-    { id: 1, name: "강아지" },
-    { id: 2, name: "고양이" },
-    { id: 3, name: "댕댕이" },
-    { id: 4, name: "하마" },
-    { id: 5, name: "뮤" },
-    { id: 6, name: "기린" },
-    { id: 7, name: "라이츄" },
-    { id: 8, name: "피카츄" },
-    { id: 9, name: "파이리" },
-]);
+    type Test = { id : number, name : string}
 
-const [add, setAdd] = useRecoilState(userData);
+    const [select, setelect] = useState<Test[]>([])
+    const [animal, setAnimal] = useState([
+        { id: 1, name: "강아지" },
+        { id: 2, name: "고양이" },
+        { id: 3, name: "댕댕이" },
+        { id: 4, name: "하마" },
+        { id: 5, name: "뮤" },
+        { id: 6, name: "기린" },
+        { id: 7, name: "라이츄" },
+        { id: 8, name: "피카츄" },
+        { id: 9, name: "파이리" },
+    ]);
+    const user = useRecoilValue(userData)
 
-const [test, setTest] = useState([{
-
-}])
-
-
-useEffect(() => {
-    loginApi.getCategory();
-}, [])
-
-// const Void = (a : any) => {
-//     console.log(a);
+    //선택한거 추가하기
+    const Plus = (animal : any) => {        
+        setelect(select.concat(animal))
+    }
     
-//     setTest([...test, a])
-// }
+    useEffect(() => {
+        getCategoryApi();
+    }, [])
 
-const example = {}
+    const example = {}
 
-return (
-    <>
-        <div className={cx('sort')}>
-            <h1 className={cx('name')}>카테고리</h1>
-        </div>
-        <div className={cx('input_containar')}>
-            <input className={cx('category_input')} placeholder="동물이름을 입력해주세요" />
-            <button>검색</button>
-        </div>
-        <div className={cx('category_containar')}>
-            {animal.map((a) => {
-                return (
-                    <div onClick={() => setTest([...test, a])} 
-                        className={cx('category_box')} key={a.id}>
-                        <p>{a.name}</p>
-                    </div>
-                )
-            })}
-        </div>
-        <div className={cx('sort')}>
-            <button onClick={()=> {loginApi.signup(example); navigate(`/main`)}} className={cx('btn')}>선택 완료</button>
-        </div>
-    </>
-)
+    return (
+        <>
+            <div className={cx('sort')}>
+                <h1 className={cx('name')}>카테고리</h1>
+            </div>
+            <div className={cx('input_containar')}>
+                <input className={cx('category_input')} placeholder="동물이름을 입력해주세요" />
+                <button>검색</button>
+            </div>
+            <div className={cx('category_containar')}>
+                {animal.map((animal) => {
+                    return (
+                        <div onClick={()=> {Plus(animal)}}
+                            className={cx('category_box')} key={animal.id}>
+                            <p>{animal.name}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className={cx('sort')}>
+                <button onClick={() => { signupApi(example) }} className={cx('btn')}>선택 완료</button>
+            </div>
+        </>
+    )
 }
 
 export default Category
