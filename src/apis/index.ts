1bf066fd821365e21ef4fa041b2f7b773cf119d0
윤선용 <sunyong9059@naver.com>
2023-07-29 23:28:27 +0900

    api 수정으로 인한 인스턴스 임시 중지

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { isExpired } from "react-jwt";
+// import { isExpired } from "react-jwt";
 
 const BASE = process.env.REACT_APP_SERVER;
 
@@ -10,26 +10,26 @@ const instance = axios.create({
   },
 });
 
-instance.interceptors.request.use(async (config) => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (!accessToken) return config;
+// instance.interceptors.request.use(async (config) => {
+//     const accessToken = localStorage.getItem("accessToken");
+//     if (!accessToken) return config;
     
-    if (isExpired(accessToken)) {
-      try {
-        const refreshToken = localStorage.getItem("refreshToken");
-        const respense = await axios.post(process.env.REACT_APP_SERVER + `/auth/token`, { refreshToken });
-        localStorage.setItem("accessToken", respense.data.accessToken);
+//     if (isExpired(accessToken)) {
+//       try {
+//         const refreshToken = localStorage.getItem("refreshToken");
+//         const respense = await axios.post(process.env.REACT_APP_SERVER + `/auth/token`, { refreshToken });
+//         localStorage.setItem("accessToken", respense.data.accessToken);
   
-        config.headers.Authorization = `Bearer ${respense.data.accessToken}`;
-        return config;
-      } catch (error) {
-        localStorage.clear();
-        window.location.reload();
-        return config;
-      }
-    }
-    config.headers.Authorization = `Bearer ${accessToken}`;
-    return config;
-  });
+//         config.headers.Authorization = `Bearer ${respense.data.accessToken}`;
+//         return config;
+//       } catch (error) {
+//         localStorage.clear();
+//         window.location.reload();
+//         return config;
+//       }
+//     }
+//     config.headers.Authorization = `Bearer ${accessToken}`;
+//     return config;
+//   });
 
 export default instance;
