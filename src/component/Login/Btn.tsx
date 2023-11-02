1eb93d727b6862ad156c272e6d33fe7819ed0865
윤선용 <sunyong9059@naver.com>
2023-11-02 22:51:18 +0900

    Feat : 버튼 nav 기능 추가

--- src/component/Login/Btn.tsx
+++ src/component/Login/Btn.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import className from "classnames/bind";
 import styles from './Btn.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 type IProps = {
   title: string;
+  nav: string;
 };
 
 const Btn = (props: IProps) => {
   const cx = className.bind(styles);
+  const navigate = useNavigate();
 
   return (
     <>
       {
         props.title === "로그인" ?
-          <div className={cx("purpleBtn")}>{props.title}
-          </div> :
-           <div className={cx("whiteBtn")}>{props.title}
-           </div>
+          <button onClick={()=> navigate(`/${props.nav}`)} className={cx("purpleBtn")}>{props.title}
+          </button> :
+           <button type='button' onClick={()=> navigate(`/${props.nav}`)} className={cx("whiteBtn")}>{props.title}
+           </button>
       }
     </>
   )
